cb64866f48083092a23529f8defa095f5b1f05fa
Ankit Gupta <guptankit.2003@gmail.com>
2025-12-22 20:10:16 +0530

    Update dependencies and environment configurations; upgrade cloudinary to v2.8.0, adjust API base URL handling, and add .env files for backend

---
+++ shofy-backend/pass.js
@@ -0,0 +1,19 @@
+const bcrypt = require("bcryptjs");
+
+bcrypt.hash("User@123", 10).then(console.log);
+
+/*
+Admin@123
+
+CEO -> palmer@gmail.com
+Manager -> meikle@gmail.com
+Admin -> harendramalik844@gmail.com
+Super Admin-> nktech123@gmail.com
+*/
+
+/*
+User@123
+
+riyapandey.nktech@gmail.com
+nktech.khushi@gmail.com
+*/
